e0c658a365905a5fd46f27d347a2ec530743232c
Student <example@example.com>
2019-09-18 11:20:13 -0700

    It changes the view with props

---
+++ src/Movie.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react';
+
+const Movie = (props) => {
+
+  return(
+    <div>
+      <h3>{props.name}</h3>
+      <h3>{props.price}</h3>
+    </div>
+  );
+}
+
+export default Movie;
