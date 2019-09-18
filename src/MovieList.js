8733ed20c8c5585cc90a0efaceef366b91f56ec4
Student <example@example.com>
2019-09-18 13:08:45 -0700

    It implements state

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,19 +1,17 @@
-import React,{useState} from 'react';
+import React,{useState, useContext} from 'react';
 import Movie from './Movie';
+import {MovieContext} from './MovieContext'
 
 const MovieList = () => {
-
-  const [movies, setMovies] = useState([
-    {name: 'Harry Potter', price: "$10", id:23124},
-    {name: 'Game of Thrones', price: "$10", id:2566124},
-    {name: 'Inception', price: "$10", id: 23524}
-  ]);
+  const value= useContext(MovieContext);
 
   return(
     <div>
-    {movies.map(movie => (
+      <h1>{value}</h1>
+
+    {/* {movies.map(movie => (
       <Movie name={movie.name} price={movie.price} key={movie.id}/>
-    ))}
+    ))} */}
     </div>
   );
 }
