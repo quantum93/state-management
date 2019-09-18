a8955bc5e29aa1dccaae869797665aa8d04e1ed3
Student <example@example.com>
2019-09-18 13:21:04 -0700

    It adds addMovie component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Nav from './Nav'
 import MovieList from './MovieList'
 import {MovieProvider} from './MovieContext';
+import AddMovie from './AddMovie';
 
 function App() {
   return (
     <MovieProvider>
       <div className="App">
         <Nav />
+        <AddMovie />
         <MovieList />
       </div>
   </MovieProvider>
